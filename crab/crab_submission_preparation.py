dcf87694233aaba798a2c3fd3b6183c359350c5b
Gouranga Kole <gouranga.tifr@gmail.com>
2023-09-06 10:17:57 +0200

    crab submission script update

--- crab/crab_submission_preparation.py
+++ crab/crab_submission_preparation.py
@@ -13,22 +13,20 @@ def join_l(l, sep):
 
 def perform_operation(action="on"):
     # Replace this with the operation you want to perform
+    l = os.environ["CMSSW_BASE"].split("/")[1:-1]
+    location = join_l(l, "/")
     if action=="on":
         print("moving .git folder to before CMSSW dir...")
-        l = os.environ["CMSSW_BASE"].split("/")[1:-1]
-        location = join_l(l, "/")
         print ("\033[1;33mmv ../.git "+location+"/\033[0;m")
         if not os.path.isdir("../.git"):
-            print ("Already moved to "+location+"/"+"  -> go ahead with crab submission")
+            print ("\033[92mAlready moved to "+location+"/"+"  -> go ahead with crab submission\033[00m")
         else:
             os.system("mv ../.git "+location+"/") 
     elif action=="off":
         print ("moving .git folder back to analysis dir")
-        l = os.environ["CMSSW_BASE"].split("/")[1:-1]
-        location = join_l(l, "/")
         print ("\033[1;33mmv "+location+"/.git "+"../\033[0;m")
         if not os.path.isdir(location+"/.git"):
-           print ("Already moved to Analysis dir")
+           print ("\033[92mAlready moved to Analysis dir\033[00m")
         else:
             os.system("mv "+location+"/.git "+"../")
  
