fdf1d4b310530536298e208be0974c54b111136c
Gouranga Kole <gouranga.tifr@gmail.com>
2024-01-31 18:39:18 +0100

    color output fixed

--- crab/check_crab_status.py
+++ crab/check_crab_status.py
@@ -37,7 +37,8 @@ for filen in filesfolders:
            
            finalDir = getFinalCRABDir(opts, filen)
            if finalDir != None:
-              print (colors.colordict["GREEN"] + "Checking status for crab directory: " + colors.colordict['CEND'], colors.colordict['BLUE'] + finalDir + colors.colordict['CEND'])
+              print (colors.colordict["GREEN"] + "Checking status for crab directory: " + colors.colordict['CEND']), 
+              print (colors.colordict['BLUE'] + finalDir + colors.colordict['CEND'])
            
               print(os.popen("crab status -d "+filen).read())
               print (50*"=")
